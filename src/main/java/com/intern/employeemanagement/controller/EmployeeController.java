f53b2ea4ff45f05766a81485664878686fe1020b
diemly26 <letrandiemly26@gmail.com>
2025-11-14 16:51:38 +0700

    module 5: exception handling

--- src/main/java/com/intern/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/intern/employeemanagement/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.intern.employeemanagement.repository.DepartmentRepository;
 import com.intern.employeemanagement.repository.EmployeeRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.intern.employeemanagement.exception.ResourceNotFoundException;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -29,8 +31,24 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> createEmployee(@Valid @RequestBody Employee employee) {
         return ResponseEntity.ok(employeeRepository.save(employee));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee was found with ID: " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee was found with ID: " + id));
+
+        employeeRepository.delete(employee);
+        return ResponseEntity.ok("Delete successfully with ID: " + id);
     }
 
 }
